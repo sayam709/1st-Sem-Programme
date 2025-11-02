b2497857622379f9960e0c71a15a0af3174a12f4
Sayam709 <142747472+sayam709@users.noreply.github.com>
2025-11-03 00:22:27 +0500

    Auto update

--- C++ Programme.cpp
+++ C++ Programme.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 
-	int		age = 17;  //4 bytes
+	int		age = 20;  //4 bytes
 	float	salary = 45.568;   // 4 bytes
 	double  distance = 1235.548;   // 8 bytes
 	char	grade = '1';  // 1 byte 0 1 T F A-Z
